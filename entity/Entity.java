f4ac69cd24a0d048c6cc2b682ed843cebeea06c3
eyob13 <79558923+eyob13@users.noreply.github.com>
2021-11-30 18:05:19 -0800

    entity code
    this the entity subprogram

---
+++ entity/Entity.java
@@ -0,0 +1,66 @@
+package Mario.entity;
+
+import java.awt.Graphics;
+
+import Game.Id;
+
+public class Entity {
+
+	public int x,y;
+	public int width, height;
+	
+	public boolean solid;
+	public int velX, vely;
+	public Id id;
+	
+	public Entity(int x, int y, int width, int height, boolean solid, Id id) {
+	    this.x = x;
+	    this.y = y;
+	    this.width = width;
+	    this.height = height;
+	    this.solid = solid;
+	    this.id = id;
+	    
+	}
+	public void render(Graphics g) {
+		
+	}
+	public void tick() {
+		x+=velX;
+		y+=vely;
+	}
+	
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	}
+	public boolean isSolid() {
+		return solid;
+	}
+	public void setSolid(boolean solid) {
+		this.solid = solid;
+	}
+	public int getVelX() {
+		return velX;
+	}
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+	public int getVely() {
+		return vely;
+	}
+	public void setVely(int vely) {
+		this.vely = vely;
+	}
+}
