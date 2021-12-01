afb824b38abc117a9de105f04ce677b15f53ad7e
eyob13 <79558923+eyob13@users.noreply.github.com>
2021-11-30 18:09:05 -0800

    buffer and graphics of the main java file
    Graphics

---
+++ Game.java
@@ -0,0 +1,101 @@
+package Game;
+
+import javax.swing.JFrame;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.Canvas;
+import java.awt.Color;
+
+public class Game extends Canvas implements Runnable {
+public static final int WIDTH = 270;
+public static final int HEIGHT= WIDTH/14*10;
+public static final int SCALE = 4;
+private static final String TITLE = "Mario";
+
+private Thread thread;
+private boolean running = false;
+
+public synchronized void start() {
+	if(running) return;
+	running = true;
+	thread = new Thread(this, "Thread");
+	thread.start();
+	
+}
+public synchronized void stop() {
+	if(running) return;
+	running = false;
+	try {
+		thread.join();
+	}catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+}
+public void run() {
+	long lastTime = System.nanoTime();
+	long timer = System.currentTimeMillis();
+	double delta = 0;
+	double ns = 1000000000.0/60.0;
+	int frames = 0;
+	int ticks = 0;
+	
+	while (running) {
+	long now = System.nanoTime();
+	delta+=(now-lastTime)/ns;
+	lastTime = now;
+	while(delta>=1) {
+		tick();
+		ticks++;
+		delta--;
+	}
+	render();
+	frames++;
+	if(System.currentTimeMillis()-timer>1000) {
+		timer+=1000;
+		System.out.println(frames + "Frames per Second" + ticks + "updates per Second");
+		frames = 0;
+		ticks = 0;
+	}
+	}
+	stop();
+}
+public void render() {
+BufferStrategy bs = getBufferStrategy();
+if(bs==null) {
+	createBufferStrategy(3);
+	return;
+	
+}
+Graphics g = bs.getDrawGraphics();	
+g.setColor(new Color(124,28,97));
+g.fillRect(0,0,getWidth(), getHeight());
+g.setColor(Color.BLUE);
+g.fillRect(200,200, getWidth()-400,getHeight()-400);
+g.dispose();
+bs.show();
+}
+
+public void tick() {
+	
+}
+public Game() {
+	Dimension size = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
+	setPreferredSize(size);
+	setMaximumSize(size);
+	setMinimumSize(size);
+}
+public static void main(String[] args) {
+	Game game = new Game();
+	JFrame frame = new JFrame(TITLE);
+	frame.add(game);
+	frame.pack();
+	frame.setResizable(false);
+	frame.setLocationRelativeTo(null);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	game.start();
+}
+}
+
